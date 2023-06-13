3fa36a2794b726e026f29d1e8155526b5b630858
Antoine F <45461034+Arrrlinks@users.noreply.github.com>
2023-06-13 15:31:31 +0200

    change: removed the :

--- YearCompletion/script.js
+++ YearCompletion/script.js
@@ -9,22 +9,22 @@ function time(){
     const yearDiv = document.getElementById("yearProgression");
     yearDiv.style.width = YearPourcent + "%";
     const yearSpan = document.getElementById("yearTxt");
-    yearSpan.innerHTML = `Year: <br>${YearPourcent}%`;
+    yearSpan.innerHTML = `Year <br>${YearPourcent}%`;
     const month = today.getMonth();
     const monthPourcent = ((((today - new Date(today.getFullYear(), month, 1)) / (1000 * 60 * 60 * 24)) +1) / 31 * 100).toFixed(2);
     const monthDiv = document.getElementById("monthProgression");
     monthDiv.style.width = monthPourcent + "%";
     const monthSpan = document.getElementById("monthTxt");
-    monthSpan.innerHTML = `Month: <br>${monthPourcent}%`;
+    monthSpan.innerHTML = `Month <br>${monthPourcent}%`;
     const dayOfTheWeek = today.getDay()-1;
     const weekPourcent = ((((dayOfTheWeek*24) + today.getHours()) / 168) * 100).toFixed(2);
     const weekDiv = document.getElementById("weekProgression");
     weekDiv.style.width = weekPourcent + "%";
     const weekSpan = document.getElementById("weekTxt");
-    weekSpan.innerHTML = `Week: <br>${weekPourcent}%`;
+    weekSpan.innerHTML = `Week <br>${weekPourcent}%`;
     const dayPourcent = (((today.getSeconds() + (today.getMinutes()*60) + (today.getHours()*3600)) / 86400) * 100).toFixed(2);
     const dayDiv = document.getElementById("dayProgression");
     dayDiv.style.width = dayPourcent + "%";
     const daySpan = document.getElementById("dayTxt");
-    daySpan.innerHTML = `Day: <br>${dayPourcent}%`;
+    daySpan.innerHTML = `Day <br>${dayPourcent}%`;
 }
